a8ca0256db8e314bcfe472acb758d22dc9c15314
manish2407 <manishms2407@gmail.com>
2022-08-31 08:51:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/first_application/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.first_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Lifecycle event: ", "In onCreate()");
+    }
+}
