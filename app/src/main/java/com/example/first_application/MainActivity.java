4871cc689a5799f200f2020e9fef79c5d6b4aaed
manish2407 <manishms2407@gmail.com>
2022-08-31 16:01:56 +0530

    Log.d function to see where you are at specific time in your application

--- app/src/main/java/com/example/first_application/MainActivity.java
+++ app/src/main/java/com/example/first_application/MainActivity.java
@@ -14,4 +14,41 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("Lifecycle event: ", "In onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Lifecycle event: ", "In onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle event: ", "In onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Lifecycle event: ", "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Lifecycle event: ", "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle event: ", "In onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Lifecycle event: ", "In onRestart()");
+    }
+
 }
